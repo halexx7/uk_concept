dbc2de71572d4eb5d6d97fc2fce6e9de5755025f
halexx7 <halexx7@gmail.com>
2021-04-05 12:30:15 +0300

    fORMS. Step_1

--- invoice/views.py
+++ invoice/views.py
@@ -80,47 +80,47 @@ class InvoiceViews(ListView):
         return context
 
 
-# Расчет КОНСТАНТНЫХ платежей (по сигналу)
-def get_calc_const():
-    users = User.objects.select_related()
-    rate = Services.get_const_payments(1)
+# # Расчет КОНСТАНТНЫХ платежей (по сигналу)
+# def get_calc_const():
+#     users = User.objects.select_related()
+#     rate = Services.get_const_payments(1)
 
-    for user in users:
-        data = dict()
-        for el in rate:
-            if el.unit == 'м2':
-                data[el.name] = [el.rate * user.appartament.sq_appart, el.unit]
-            elif el.unit == 'чел':
-                data[el.name] = [el.rate * user.appartament.num_owner, el.unit]
-            else:
-                data[el.name] = [el.rate, el.unit]
-        user_id = User.objects.get(id=user.id)
-        record = ConstantPayments(user=user_id, data=json.dumps(
-            data, ensure_ascii=False, default=str))
-        record.save()
-    return data
+#     for user in users:
+#         data = dict()
+#         for el in rate:
+#             if el.unit == 'м2':
+#                 data[el.name] = [el.rate * user.appartament.sq_appart, el.unit]
+#             elif el.unit == 'чел':
+#                 data[el.name] = [el.rate * user.appartament.num_owner, el.unit]
+#             else:
+#                 data[el.name] = [el.rate, el.unit]
+#         user_id = User.objects.get(id=user.id)
+#         record = ConstantPayments(user=user_id, data=json.dumps(
+#             data, ensure_ascii=False, default=str))
+#         record.save()
+#     return data
 
 
-# Расчет ПЕРЕМЕННЫХ платежей (по сигналу)
-def get_calc_variable():
-    users = User.objects.select_related()
-    curr = CurrentCounter.objects.get(id=1)
-    hist = HistoryCounter.get_last_val(1)[0]
-    rate = Services.get_varybose_payments(1)
+# # Расчет ПЕРЕМЕННЫХ платежей (по сигналу)
+# def get_calc_variable():
+#     users = User.objects.select_related()
+#     curr = CurrentCounter.objects.get(id=1)
+#     hist = HistoryCounter.get_last_val(1)[0]
+#     rate = Services.get_varybose_payments(1)
     
 
-    for user in users:
-        data = dict()
-        user_id = User.objects.get(id=user.id)
+#     for user in users:
+#         data = dict()
+#         user_id = User.objects.get(id=user.id)
 
-        for el in rate:
-            if el.name == 'Холодная вода (индивидуальное потребление)':
-                value = el.rate * (curr.col_water - hist.hist_col_water )
-            elif el.name == 'Горячая вода (индивидуальное потребление)':
-                value = el.rate * (curr.hot_water - hist.hist_hot_water )
-            elif el.name == 'Электроэнергия день':
-                value = el.rate * (curr.electric_day - hist.hist_electric_day )
-            elif el.name == 'Электроэнергия ночь':
-                value = el.rate * (curr.electric_night - hist.hist_electric_night )
-            record = VariablePayments(user=user_id, period='2021-02-04', service=el.name, price=value)
-            record.save()
+#         for el in rate:
+#             if el.name == 'Холодная вода (индивидуальное потребление)':
+#                 value = el.rate * (curr.col_water - hist.hist_col_water )
+#             elif el.name == 'Горячая вода (индивидуальное потребление)':
+#                 value = el.rate * (curr.hot_water - hist.hist_hot_water )
+#             elif el.name == 'Электроэнергия день':
+#                 value = el.rate * (curr.electric_day - hist.hist_electric_day )
+#             elif el.name == 'Электроэнергия ночь':
+#                 value = el.rate * (curr.electric_night - hist.hist_electric_night )
+#             record = VariablePayments(user=user_id, period='2021-02-04', service=el.name, price=value)
+#             record.save()
